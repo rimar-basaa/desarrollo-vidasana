8374426ec4b46753362bdbba0b3fd66188a9780c
rimar basaa <rimar.basaa@gmail.com>
2024-02-17 19:11:45 -0300

    se agrega validacion de credencial

--- consultas.js
+++ consultas.js
@@ -8,16 +8,24 @@ const pool = new Pool({
     allowExitOnIdle: true
 })
 
+const verificarCredenciales = async (email, password) => {
+    const consulta = "SELECT * FROM usuarios WHERE email = $1 AND password = $2";
+    const values = [email, password];
+    const { rowCount } = await pool.query(consulta, values);
+    if (!rowCount)
+        throw {code: 404, message: "NO se encontro ningun usuario con esta credencial"};
+};
+
 const getEventos = async () => {
-    const { rows: eventos } = await pool.query("SELECT * FROM eventos")
-    return eventos
-}
+    const { rows: eventos } = await pool.query("SELECT * FROM eventos");
+    return eventos;
+};
 
 const deleteEvento = async (id) => {
-    const consulta = "DELETE FROM eventos WHERE id = $1"
-    const values = [id]
-    const { rowCount } = await pool.query(consulta, values)
-    if (!rowCount) throw { code: 404, message: "No se encontró ningún evento con este ID" }
-}
+    const consulta = "DELETE FROM eventos WHERE id = $1";
+    const values = [id];
+    const { rowCount } = await pool.query(consulta, values);
+    if (!rowCount) throw { code: 404, message: "No se encontró ningún evento con este ID" };
+};
 
-module.exports = { getEventos, deleteEvento }
+module.exports = { getEventos, deleteEvento, verificarCredenciales };
--- index.js
+++ index.js
@@ -1,17 +1,35 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const { getEventos, deleteEvento } = require('./consultas')
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const morgan = require('morgan')
+const { getEventos, deleteEvento, verificarCredenciales } = require('./consultas');
 
-app.listen(3000, console.log("servidor corriendo... en puerto:3000"))
-app.use(cors())
-app.use(express.json())
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
 
 app.get("/eventos", async (req, res) => {
     try {
-        const eventos = await getEventos()
-        res.json(eventos)
+        const eventos = await getEventos();
+        res.json(eventos);
     } catch (error) {
-        res.status(error.code || 500).send(error)
-    }
-})
+        res.status(error.code || 500).send(error);
+    };
+});
+
+app.post("/login", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        await verificarCredenciales(email, password);
+        const token = jwt.sign({ email }, "az_AZ");
+        res.send(token);
+    } catch (error) {
+        console.log(error);
+        res.status(error.code || 500).send(error);
+    };
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, console.log(`servidor corriendo... en puerto:${PORT}`));
