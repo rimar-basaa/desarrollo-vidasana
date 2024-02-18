7af892afed2d3ee40cd36bb422511dd4742ee829
rimar basaa <rimar.basaa@gmail.com>
2024-02-18 10:21:12 -0300

    agrega delete con token

--- index.js
+++ index.js
@@ -23,10 +23,38 @@ app.post("/login", async (req, res) => {
     try {
         const { email, password } = req.body;
         await verificarCredenciales(email, password);
-        const token = jwt.sign({ email }, "az_AZ");
+        const token = jwt.sign({ email }, "rimar");
         res.send(token);
     } catch (error) {
         console.log(error);
+        res.status(error.code || 500).send(error);
+    };
+});
+
+app.delete("/eventos/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const Authorization = req.header('Authorization');
+        const token = Authorization.split(' ')[1];
+        jwt.verify(token, "rimar");
+        const { email } = jwt.decode(token);
+        await deleteEvento(id);
+        res.send(`El usuario ${email} ha eliminado el evento con id:${id}`);
+    } catch (error) {
+        res.status(error.code || 500).send(error);
+    };
+});
+
+app.put("/eventos/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const data = req.body;
+        const Authorization = req.header('Authorization');
+        const token = Authorization.split(' ')[1];
+        console.log(id);
+        console.log(data);
+        console.log(token);
+    } catch (error) {
         res.status(error.code || 500).send(error);
     };
 });
